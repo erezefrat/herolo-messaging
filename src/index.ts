7f0cccbfd02f067ed84c07cb25dfa27c563100c8
erez.efrat <erez.efrat@overwolf.com>
2020-07-06 10:55:04 +0300

    port

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const start = async () => {
     console.error(err);
   }
 
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000');
   });
 };
